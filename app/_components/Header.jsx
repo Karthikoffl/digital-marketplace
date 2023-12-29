d57a068c4495718f21a44e5e59474c647c6fe8c1
Resources-DTW <resource.dtw@gmail.com>
2023-12-29 13:05:38 +0530

    init

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Header() {
   return (
     <header className="bg-white">
-      <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto flex shadow-sm h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
         <Image src="/logo.svg" alt="Logo" width={80} height={100} />
 
         <div className="flex flex-1 items-center justify-end md:justify-between">
@@ -60,14 +60,14 @@ function Header() {
           <div className="flex items-center gap-4">
             <div className="sm:flex sm:gap-4">
               <a
-                className="block rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
+                className="block rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white transition hover:bg-blue-600"
                 href="/"
               >
                 Login
               </a>
 
               <a
-                className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
+                className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-primary transition hover:text-blue-600/75 sm:block"
                 href="/"
               >
                 Register
